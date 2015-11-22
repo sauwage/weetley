0aa30d4f051f650d6aa71f675fa3ab887fc8b283
jenis <jenis@avast.com>
2015-11-23 00:59:23 +0100

    first initial commit

---
+++ gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            all: {
+                files: {
+                    "./dist/css/main.css": "./src/less/main.less"
+                }
+            }
+        },
+
+        cssmin: {
+          build: {
+            files: {
+              'dist/css/main.min.css': 'dist/css/main.css'
+            }
+          }
+        },
+
+        watch: {
+            less: {
+                files: ['**/*.less'],
+                tasks: ['less', 'cssmin'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['less', 'cssmin']);
+};
