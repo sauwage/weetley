f521b2b5cdddb374cfcd53c70023fdb7ea1e760a
jenis <jenis@avast.com>
2016-04-11 17:44:00 +0200

    working small tiles

--- src/js/app.js
+++ src/js/app.js
@@ -12,46 +12,57 @@ var weatherJson = require('./icons.json');
     };
 
     var app = angular.module('weetley',[]);
-    app.controller('weatherReport', function($scope, $http, $timeout){
+    app.controller('weatherReport', function($scope, $http, $timeout, $q){
         $scope.townsNames = [];
 
         $scope.addTown = function(city){
-            if(city == '') {
+
+            if(!city) {
                 $scope.required = {
                     "border" : "2px dotted red"
-                }
+                };
+                return false;
             } else {
                 $scope.required = {
                     "border" : "none"
-                };
-                $http({
-                    url: 'http://api.openweathermap.org/data/2.5/weather',
-                    method: "GET",
-                    params: {
-                        q : city,
-                        units: 'metric',
-                        appid : '8d37df734b83981d7ad5e4f21c004aa2'
-                    }
-                }).success(function(data){
-                    $scope.townsNames.push({
-                        id: translateIcon(data.weather[0].id),
-                        name: data.name,
-                        temp: data.main.temp
-                    });
-                }).error(function(){
-                    $timeout(function(){
-                        $scope.addTown(city);
-                    }, 1000);
-                });
-            }
+                }
+            };
 
+            $http.get('http://api.openweathermap.org/data/2.5/forecast',{params: {appid : '8d37df734b83981d7ad5e4f21c004aa2',q : city,units: 'metric'}})
+            .then(function(results) {
+                $scope.myArray = results;
+                var forecast = [];
+
+                for(var i = 11; i <= 35; i += 8) {
+                    console.log(i);
+                    var newDateObject = new Date($scope.myArray.data.list[i].dt*1000).toLocaleDateString();
+                    var newIcon = translateIcon($scope.myArray.data.list[i].weather[0].id);
+                    var temp = $scope.myArray.data.list[i].main.temp.toFixed(1);
+                    forecast.push({
+                        date: newDateObject,
+                        icon: newIcon,
+                        temp: temp
+                    });
+                };
+                $scope.townsNames.push({
+                    name : $scope.myArray.data.city.name,
+                    icon_today : translateIcon($scope.myArray.data.list[3].weather[0].id),
+                    temp_today : $scope.myArray.data.list[3].main.temp.toFixed(1),
+                    forecast : forecast
+                });
+                console.log($scope.townsNames);
+            }, function(reason){
+                $timeout(function(){
+                    $scope.addTown(city);
+                }, 500);
+            });
             $scope.townToBeAdded = "";
         };
 
         $scope.init = function() {
+            $scope.addTown('Tokyo');
             $scope.addTown('Berlin');
-            $scope.addTown('Prague');
-            $scope.addTown('Stockholm');
+            $scope.addTown('San Francisco');
         };
 
         $scope.removeTown = function(x){
